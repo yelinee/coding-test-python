933bc300eacf4b1a3cb3a174c80d97bb47916b16
Replit user <>
2022-11-11 09:43:43 +0000

    Add Readme.txt

---
+++ greedy/backjun_1781.py
@@ -0,0 +1,18 @@
+# 2109번과 동일한 방식으로 풀이
+import heapq
+
+n = int(input())
+lst = []
+for i in range(n):
+  lst.append(list(map(int, input().split())))
+
+lst.sort(key=lambda x: x[0])
+
+p_list = []
+for i in lst:
+  heapq.heappush(p_list, i[1])
+
+  if (len(p_list) > i[0]):
+    heapq.heappop(p_list)
+
+print(sum(p_list))
